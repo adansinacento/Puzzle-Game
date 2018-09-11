d0ae4218ad394e00a45c2c9033d20d4266a29405
Adan <adansinacento@gmail.com>
2018-09-11 15:37:23 -0500

    Movimiento baaaasico

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    private Vector3 pos;
+    private Transform tr;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        speed = 2;
+        pos = transform.position;
+        tr = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.D) && tr.position == pos)
         {
-            transform.Translate(0, 0, 1);
+            pos += Vector3.right;
         }
-	}
+        else if (Input.GetKeyDown(KeyCode.A) && tr.position == pos)
+        {
+            pos += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && tr.position == pos)
+        {
+            pos += Vector3.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && tr.position == pos)
+        {
+            pos += Vector3.back;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
 }
