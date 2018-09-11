eba1da712436996e029824d2444738971d0f2f6d
Rubén Ostos <reflejodevapor@gmail.com>
2018-09-11 16:19:55 -0500

    SaveData
    por ahora, falta adaptar ya que estén todas las cosas.

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pinguinos
+{
+	public class SaveData : MonoBehaviour
+	{
+//-----------------------------------------------
+		public int timesWon = 0;
+		public int timesLost = 0;
+		public int highestScore = 0;
+//-----------------------------------------------
+		public int extraCharA = 0;
+		public int extraCharB = 0;
+//-----------------------------------------------
+	}
+}
