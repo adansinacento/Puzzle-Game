5047ccf10fa3eab8ce1d93b4f73eef100227b028
Adan <adansinacento@gmail.com>
2018-09-11 23:04:57 -0500

    Mejorado el movimiento del personaje

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -12,8 +12,9 @@ namespace Pinguinos
         private Transform tr;
         private int posX = 0;
         private int posY = 0;
+        private bool FirstCommand = false;
 
-        private enum PossibleDirections
+        public enum PossibleDirections
         {
             Stopped,
             Left,
@@ -33,15 +34,81 @@ namespace Pinguinos
             DirectionMovement = PossibleDirections.Stopped;
         }
 
-        // Update is called once per frame
         void Update()
+        {
+            if (DirectionMovement == PossibleDirections.Stopped)
+            {
+                PossibleDirections Selected = PossibleDirections.Stopped;
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    Selected = PossibleDirections.Right;
+                }
+                else if (Input.GetKeyDown(KeyCode.A))
+                {
+                    Selected = PossibleDirections.Left;
+                }
+                else if (Input.GetKeyDown(KeyCode.W))
+                {
+                    Selected = PossibleDirections.Forward;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    Selected = PossibleDirections.Back;
+                }
+                Debug.Log("Se ha seleccionado " + Selected.ToString());
+                if (!MapOptionsUtils.IsNextSpaceAvailable(Selected, posX, posY)) return; //Si la opcion indicada no se puede ahi le cortamos
+                DirectionMovement = Selected; //Se asigna la direccion deseada
+
+                while  (!MapOptionsUtils.MustStopHere(Selected, posX, posY) && MapOptionsUtils.IsNextSpaceAvailable(Selected, posX, posY))
+                {
+                    switch (DirectionMovement)
+                    {
+                        case PossibleDirections.Left:
+                            posGO += Vector3.left;
+                            posX--;
+                            break;
+                        case PossibleDirections.Right:
+                            posGO += Vector3.right;
+                            posX++;
+                            break;
+                        case PossibleDirections.Forward:
+                            posGO += Vector3.forward;
+                            posY--;
+                            break;
+                        case PossibleDirections.Back:
+                            posGO += Vector3.back;
+                            posY++;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, posGO, Time.deltaTime * speed);
+                if (tr.position == posGO)
+                {
+                    DirectionMovement = PossibleDirections.Stopped;
+                    Debug.Log("Personaje esta en: " + posX + ", " + posY);
+                    Debug.Log("Limites: " + LevelLayout.level1.GetLength(0) + ", " + LevelLayout.level1.GetLength(1));
+                }
+                    
+            }
+            
+        }
+
+        
+        
+
+        // Update is called once per frame
+        void FakeUpdate()
         {
             //Aqui toma las decisiones
             if (tr.position == posGO)
             {
                 Debug.Log("AHHHH");
-                if (CanChangeDirection())
+                if (true)
                 {
+                    FirstCommand = true;
                     Debug.Log("Can change direction");
                     DirectionMovement = PossibleDirections.Stopped;
                     if (Input.GetKeyDown(KeyCode.D))
@@ -60,64 +127,43 @@ namespace Pinguinos
                     {
                         DirectionMovement = PossibleDirections.Back;
                     }
+
                 }
                 else
                 {
                     Debug.Log("Cant change direction");
+                    Debug.Log(DirectionMovement.ToString());
+                }
+            }
+            if (FirstCommand)
+            {
+                FirstCommand = false;
+                switch (DirectionMovement)
+                {
+                    case PossibleDirections.Left:
+                        posGO += Vector3.left;
+                        posX--;
+                        break;
+                    case PossibleDirections.Right:
+                        posGO += Vector3.right;
+                        posX++;
+                        break;
+                    case PossibleDirections.Forward:
+                        posGO += Vector3.forward;
+                        posY--;
+                        break;
+                    case PossibleDirections.Back:
+                        posGO += Vector3.back;
+                        posY++;
+                        break;
                 }
             }
             
-            switch (DirectionMovement)
-            {
-                case PossibleDirections.Left:
-                    posGO += Vector3.left;
-                    posX--;
-                    break;
-                case PossibleDirections.Right:
-                    posGO += Vector3.right;
-                    posX++;
-                    break;
-                case PossibleDirections.Forward:
-                    posGO += Vector3.back;
-                    posY--;
-                    break;
-                case PossibleDirections.Back:
-                    posGO += Vector3.forward;
-                    posY++;
-                    break;
-            }
 
             transform.position = Vector3.MoveTowards(transform.position, posGO, Time.deltaTime * speed);
         }
 
-        private bool CanChangeDirection()
-        {
-            bool IsPossible = false;
-            switch (DirectionMovement)
-            {
-                case PossibleDirections.Stopped:
-                    IsPossible = true;
-                    break;
-                case PossibleDirections.Left:
-                    IsPossible = (posX < LevelLayout.level1.GetLength(0) && TitleCausesStop(LevelLayout.level1[posX+1, posY]));
-                    break;
-                case PossibleDirections.Right:
-                    IsPossible = (posX > 0 && TitleCausesStop(LevelLayout.level1[posX - 1, posY]));
-                    break;
-                case PossibleDirections.Forward:
-                    IsPossible = (posY < LevelLayout.level1.GetLength(0) && TitleCausesStop(LevelLayout.level1[posX, posY + 1]));
-                    break;
-                case PossibleDirections.Back:
-                    IsPossible = (posY > 0 && TitleCausesStop(LevelLayout.level1[posX, posY - 1]));
-                    break;
-            }
-            return IsPossible;
-        }
-
-        private bool TitleCausesStop(MapOptions title)
-        {
-            return title == MapOptions.Wall || title == MapOptions.Rock || title == MapOptions.SteppbleFloor;
-        }
+  
     }
 }
 
--- Assets/Scripts/EnumMaps.cs
+++ Assets/Scripts/EnumMaps.cs
@@ -13,6 +13,60 @@ namespace Pinguinos
         Rock
     }
 
-    //public static class
+    public static class MapOptionsUtils
+    {
+
+        public static bool MustStopHere(CharacterMovement.PossibleDirections direction, int x, int y)
+        {
+            switch (direction)
+            {
+                case CharacterMovement.PossibleDirections.Forward:
+                    if (x + 1 == LevelLayout.level1.GetLength(0)) return true;
+                    return TitleStopsYou(LevelLayout.level1[x, y]);
+                case CharacterMovement.PossibleDirections.Back:
+                    if (x == 0) return true;
+                    return TitleStopsYou(LevelLayout.level1[x, y]);
+                case CharacterMovement.PossibleDirections.Left:
+                    if (y == 0) return true;
+                    return TitleStopsYou(LevelLayout.level1[x, y]);
+                case CharacterMovement.PossibleDirections.Right:
+                    if (y + 1 == LevelLayout.level1.GetLength(1)) return true;
+                    return TitleStopsYou(LevelLayout.level1[x, y]);
+                default:
+                    return true; //Default handler
+            }
+        }
+
+        public static bool IsNextSpaceAvailable(CharacterMovement.PossibleDirections direction, int x, int y)
+        {
+            switch (direction)
+            {
+                case CharacterMovement.PossibleDirections.Forward:
+                    if (x + 1 > LevelLayout.level1.GetLength(0)) return false;
+                    return !TitleStopsYouNext(LevelLayout.level1[x + 1, y]);
+                case CharacterMovement.PossibleDirections.Back:
+                    if (x - 1 < 0) return false;
+                    return !TitleStopsYouNext(LevelLayout.level1[x - 1, y]);
+                case CharacterMovement.PossibleDirections.Left:
+                    if (y - 1 < 0) return false;
+                    return !TitleStopsYouNext(LevelLayout.level1[x, y - 1]);
+                case CharacterMovement.PossibleDirections.Right:
+                    if (y + 1 > LevelLayout.level1.GetLength(1)) return false;
+                    return !TitleStopsYouNext(LevelLayout.level1[x, y + 1]);
+                default:
+                    return false; //Si esta detenido le decimos que no se mueva
+            }
+        }
+
+        private static bool TitleStopsYouNext(MapOptions title)
+        {
+            return title == MapOptions.Wall || title == MapOptions.Rock;
+        }
+
+        private static bool TitleStopsYou(MapOptions title)
+        {
+            return title == MapOptions.SteppbleFloor;
+        }
+    }
 }
 
