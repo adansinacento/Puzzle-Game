38562a15bea1ab5525e8d92969e68761a06e34f7
SorenGedeon <32682025+TerrierBrakk@users.noreply.github.com>
2018-09-13 13:45:27 -0500

    Pruebas sonido, y mas botones

---
+++ Assets/Scripts/Sound/PruebaSonido.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PruebaSonido : MonoBehaviour {
+
+	AudioSource audio;
+	public Slider Volumen;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		if(audio == null)
+		{
+			audio = gameObject.GetComponent<AudioSource>();
+		}
+		StartCoroutine("CambioSonido");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	IEnumerator CambioSonido()
+	{
+		yield return new WaitForSeconds(0.05f);
+		
+		audio.volume = Volumen.value;
+		
+		StartCoroutine("CambioSonido");
+	}
+	
+}
