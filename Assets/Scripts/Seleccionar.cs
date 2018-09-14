21e058961fe6464cb4603c8f8b6531c37e39d7c7
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2018-09-14 13:02:35 -0500

    Seleccionar Personaje

---
+++ Assets/Scripts/Seleccionar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Seleccionar : MonoBehaviour {
+	
+	public GameObject[] Personajes;
+	
+	static int personaje;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(SceneManager.GetActiveScene().name == "Prueba_Personajes")
+		{
+			Instantiate(Personajes[personaje], transform.position, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	public void SelectPersonaje(int Personaje)
+	{
+		personaje = Personaje;
+	}
+	
+	public void CambiarEscena()
+	{
+		SceneManager.LoadScene ("Prueba_Personajes");
+	}
+}
