0d165dd2cee1199fe6fc0db59487793204e45c2d
Rubén Ostos <reflejodevapor@gmail.com>
2018-09-20 08:14:53 -0500

    ssfsgfs
    fdf

--- Assets/Scripts/LevelLayout.cs
+++ Assets/Scripts/LevelLayout.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,15 +10,71 @@ namespace Pinguinos
     public class LevelLayout
     {
         // En teoría cargariamos esto de un .json si tuvieramos el level creator
-        public static MapOptions[,] level1 = new MapOptions[,] 
+        public static MapOptions[,] level_1 = new MapOptions[,] 
         { 
-            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice },
-            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice },
-            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice },
-            { MapOptions.Rock, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice }
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Rock, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Rock,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Rock, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice /*Este celda va un item.*/,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.SteppbleFloor,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor}        
+        
         };
 
-        public static MapOptions[,] ActualLevel = level1; 
+        /*
+        public static MapOptions[,] level_1 = new MapOptions[,] 
+        { 
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+        { MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Rock, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Rock,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Rock, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice *Este celda va un item.*,MapOptions.Ice},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+        { MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.SteppbleFloor,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+        { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor}        
+        
+        };
+
+
+        
+         */
+
+
+
+        public static MapOptions[,] level_2 = new MapOptions[,] 
+        { 
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Rock, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Rock,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.SteppbleFloor, MapOptions.Rock, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice /*Este celda va un item.*/,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+      /**/  { MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+            { MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.SteppbleFloor,MapOptions.Ice,MapOptions.Ice,MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice,MapOptions.Ice,MapOptions.Ice,MapOptions.SteppbleFloor}        
+        
+        };
+
+        public static MapOptions[,] level_Tutorial = new MapOptions[,] 
+        { 
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice},
+            { MapOptions.Rock, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Warp,  MapOptions.Hole, MapOptions.Warp, MapOptions.SteppbleFloor, MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice},
+            { MapOptions.Ice, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.Ice, MapOptions.Ice},
+            { MapOptions.SteppbleFloor, MapOptions.Ice, MapOptions.Ice,  MapOptions.Ice, MapOptions.Ice, MapOptions.SteppbleFloor, MapOptions.Ice}
+        };
+
+
+        public static MapOptions[,] ActualLevel = level_1; 
     }
 }
 
